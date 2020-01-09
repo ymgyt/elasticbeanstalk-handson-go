2f927ba3cb91ec95472c25855f2057decd906d70
ymgyt <yamaguchi7073xtt@gmail.com>
2020-01-09 22:00:37 +0900

    add dump path

--- application.go
+++ application.go
@@ -12,18 +12,22 @@ const addr = ":5000"
 
 func main() {
 	mux := 	http.NewServeMux()
-	mux.HandleFunc("/dump", func(w http.ResponseWriter, r *http.Request){
-		dump, err := httputil.DumpRequest(r, true)
-		if err != nil {
-			io.WriteString(w, err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Write(dump)
-		w.WriteHeader(http.StatusOK)
-	})
+	mux.HandleFunc("/dump", dump)
+	mux.HandleFunc("/", dump)
+	mux.HandleFunc("", dump)
 
 	s := &http.Server{Addr: addr,  Handler: mux}
 	fmt.Println("running on ", addr)
 	fmt.Fprintln(os.Stderr, s.ListenAndServe())
+}
+
+func dump(w http.ResponseWriter, r *http.Request){
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(dump)
+	w.WriteHeader(http.StatusOK)
 }
