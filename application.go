bb2af19d9b991f44bdb5c367fe2ca51bf12b52c1
ymgyt <yamaguchi7073xtt@gmail.com>
2020-01-09 23:21:07 +0900

    add env handler

--- application.go
+++ application.go
@@ -6,14 +6,17 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"sort"
+	"strings"
 )
 
 const addr = ":5000"
 
 func main() {
 	mux := 	http.NewServeMux()
-	mux.HandleFunc("/dump", dump)
 	mux.HandleFunc("/", dump)
+	mux.HandleFunc("/dump", dump)
+	mux.HandleFunc("/env", env)
 
 	s := &http.Server{Addr: addr,  Handler: mux}
 	fmt.Println("running on ", addr)
@@ -23,10 +26,17 @@ func main() {
 func dump(w http.ResponseWriter, r *http.Request){
 	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
-		io.WriteString(w, err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, err.Error())
 		return
 	}
 	w.Write(dump)
-	w.WriteHeader(http.StatusOK)
+}
+
+func env(w http.ResponseWriter, r *http.Request) {
+	envs := os.Environ()
+	sort.Slice(envs, func(i,j int) bool {
+		return envs[i] < envs[j]
+	})
+	io.WriteString(w, strings.Join(envs, "\n"))
 }
